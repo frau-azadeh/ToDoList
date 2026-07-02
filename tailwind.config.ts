56ac10feceab03a67ee27fba12a9aaaf2ec566b9
A.Sharifisoltani <designweb.azadeh@gmail.com>
2026-07-02 09:40:45 +0330

    update

---
+++ tailwind.config.ts
@@ -0,0 +1,21 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "hsl(210, 100%, 98%)",
+        text: "hsl(220, 35%, 20%)",
+      },
+    },
+  },
+  plugins: [],
+};
+
+export default config;
